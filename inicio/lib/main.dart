b8f39eba06174bb4299921837b0d6d172474f8c7
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-09-26 19:02:24 -0300

    aprendendo a dinamica de arvore e widgest

--- inicio/lib/main.dart
+++ inicio/lib/main.dart
@@ -11,28 +11,127 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Stack(
-        //pai de todos
-        alignment: AlignmentDirectional.center,
-        children: [
-          //filhos
-          Container(
-            color: Colors.amber,
-            width: 200,
-            height: 200,
-          ),
-          Container(
-            color: Colors.blueGrey,
-            width: 100,
-            height: 100,
-          )
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Container(
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.amber,
+                      width: 200,
+                      height: 200,
+                    ),
+                    Container(
+                      color: Colors.blueGrey,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.redAccent,
+                      width: 200,
+                      height: 200,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.purple,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.pink,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.tealAccent,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                ),
+                Container(
+                  color: Colors.orange,
+                  height: 50,
+                  width: 300,
+                  child: Text(
+                    "natan carrapeiro",
+                    style: TextStyle(color: Colors.black, fontSize: 30),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                    //botao
+                    onPressed: () {
+                      print("botao apertado");
+                      //função ao aperta o botao
+                    },
+                    child: Text("botao"))
+                //texto do botao
+              ]),
+        ));
   }
+}
+
+//organisação dos widgets Colum /Row
+void coluna() {
+  //como se usa o Colum e suas funções
+
+  // home: Column(
+  //     mainAxisAlignment: MainAxisAlignment.center, //eixo principal
+  //     crossAxisAlignment: CrossAxisAlignment.center, //eixo secundario
+  //     children: [
+  //        //filhos
+  //     Container(
+  //           color: Colors.amber,
+  //            width: 200,
+  //           height: 200,
+  //           ),
+  //           Container(
+  //             color: Colors.blueGrey,
+  //             width: 100,
+  //             height: 100,
+  //           )
+  //         ]),
+}
+void linha() {
+  //  home: Row(
+  //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //       crossAxisAlignment: CrossAxisAlignment.center,
+  //       children: [
+  //         Container(
+  //           color: Colors.amber,
+  //           width: 200,
+  //           height: 200,
+  //         ),
+  //         Container(
+  //           color: Colors.blueGrey,
+  //           width: 200,
+  //           height: 200,
+  //         )
+  //       ],
+  //     ),
 }
