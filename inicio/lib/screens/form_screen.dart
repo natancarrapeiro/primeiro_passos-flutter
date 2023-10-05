e8952b1b5655d4a0046aaef137cdc4bbb935aeae
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-10-05 14:44:01 -0300

    verificação de dados
    usando Forme para visualisar os dados
    e key para informar o usuario de erro nos campos sem mudar o estado da tela
    
    mostrar quando da certo o envio de dados

--- inicio/lib/screens/form_screen.dart
+++ inicio/lib/screens/form_screen.dart
@@ -12,128 +12,160 @@ class _formScreenState extends State<formScreen> {
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imgUrlController = TextEditingController();
 
+  // variavel que recebe uma chave global
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.task),
-        title: const Text(
-          "Cadrastro de tarefas",
-          style: TextStyle(color: Colors.white, fontSize: 24),
+    // form ele visualisa o formulario se ele esta correto
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.task),
+          title: const Text(
+            "Cadrastro de tarefas",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          backgroundColor: Colors.blueAccent,
         ),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 780,
-            width: 400,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 2),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: nameController,
-                        textAlign: TextAlign.center,
-                        cursorColor: Colors.blueAccent,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Nome da Tarefa",
-                          fillColor: Colors.white70,
-                          filled: true,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 780,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 2),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (String? value) {
+                            if (value != null && value.isEmpty) {
+                              return 'Insira nome a Tarefa';
+                            }
+                            return null;
+                          },
+                          controller: nameController,
+                          textAlign: TextAlign.center,
+                          cursorColor: Colors.blueAccent,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Nome da Tarefa",
+                            fillColor: Colors.white70,
+                            filled: true,
+                          ),
+                          style: const TextStyle(color: Colors.black),
+                          maxLength: 30,
                         ),
-                        style: const TextStyle(color: Colors.black),
-                        maxLength: 30,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        controller: difficultyController,
-                        textAlign: TextAlign.center,
-                        cursorColor: Colors.blueAccent,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Dificuldade de 1 a 5",
-                          fillColor: Colors.white70,
-                          filled: true,
-                        ),
-                        style: const TextStyle(color: Colors.black),
-                        maxLength: 1,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        onChanged: (texto) {
-                          setState(() {});
-                        },
-                        keyboardType: TextInputType.url,
-                        controller: imgUrlController,
-                        textAlign: TextAlign.center,
-                        cursorColor: Colors.blueAccent,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Img/url",
-                          fillColor: Colors.white70,
-                          filled: true,
-                        ),
-                        style: const TextStyle(color: Colors.black),
-
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 80,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(width: 2, color: Colors.blue),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.network(
-                      imgUrlController.text,
-                      // tratarmento de exception
-                      errorBuilder: (BuildContext context, Object exception,
-                          StackTrace? stackTrace) {
-                        return Image.asset("assets/images/semImagem.png");
-                      },
-                      fit: BoxFit.cover,
+                      ],
                     ),
                   ),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      print(nameController.text);
-                      print(int.parse(difficultyController.text));
-                      print(imgUrlController.text);
-                    },
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text("Castrate")],
-                    ))
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                int.parse(value) > 5 ||
+                                int.parse(value) < 1) {
+                              return 'Insira dificuldade entre 1 a 5';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.number,
+                          controller: difficultyController,
+                          textAlign: TextAlign.center,
+                          cursorColor: Colors.blueAccent,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Dificuldade de 1 a 5",
+                            fillColor: Colors.white70,
+                            filled: true,
+                          ),
+                          style: const TextStyle(color: Colors.black),
+                          maxLength: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          onChanged: (texto) {
+                            setState(() {});
+                          },
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Insira uma URL de Imagem ";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.url,
+                          controller: imgUrlController,
+                          textAlign: TextAlign.center,
+                          cursorColor: Colors.blueAccent,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Img/url",
+                            fillColor: Colors.white70,
+                            filled: true,
+                          ),
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 80,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        imgUrlController.text,
+                        // tratarmento de exception
+                        errorBuilder: (BuildContext context, Object exception,
+                            StackTrace? stackTrace) {
+                          return Image.asset("assets/images/semImagem.png");
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        // pego a chave e verifico se ela esta tudo bem
+                        if (_formKey.currentState!.validate()) {
+                          print(nameController.text);
+                          print(int.parse(difficultyController.text));
+                          print(imgUrlController.text);
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Tarefa  criada')));
+                        }
+                      },
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text("Castrate")],
+                      ))
+                ],
+              ),
             ),
           ),
         ),
