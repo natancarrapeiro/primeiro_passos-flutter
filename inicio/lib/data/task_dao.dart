62ecd1aa3a47e648e764a3cf42eedbee2c55806f
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-10-09 20:13:15 -0300

    criada todas as funções CRUD
    find= mostrar; save=salvar ou aterar;
    delete=deletar a tarefa

--- inicio/lib/data/task_dao.dart
+++ inicio/lib/data/task_dao.dart
@@ -14,7 +14,30 @@ class TaskDao {
   static const String _difficulty = "difficulty";
   static const String _image = "image";
 
-  save(Task tarefa) async {}
+  save(Task tarefa) async {
+    print('iniciando o save:');
+    final Database bancoDeDados = await getDatabasase();
+    var itemExists = await find(tarefa.nome);
+    Map<String, dynamic> taskMap = toMap(tarefa);
+    if (itemExists.isEmpty) {
+      print('a tarefava nao existia.');
+      return await bancoDeDados.insert(_tablename, taskMap);
+    } else {
+      print("tarefa ja existe.");
+      return await bancoDeDados.update(_tablename, taskMap,
+          where: '$_name=?', whereArgs: [tarefa.nome]);
+    }
+  }
+
+  Map<String, dynamic> toMap(Task tarefa) {
+    print('convertendo tarefa em map:');
+    final Map<String, dynamic> mapaDeTarefas = Map();
+    mapaDeTarefas[_name] = tarefa.nome;
+    mapaDeTarefas[_image] = tarefa.foto;
+    mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+    print("conersao feita map : $mapaDeTarefas");
+    return mapaDeTarefas;
+  }
 
   Future<List<Task>> findAll() async {
     print("acesso findAll:");
@@ -43,9 +66,14 @@ class TaskDao {
     final Database bancoDeDados = await getDatabasase();
     final List<Map<String, dynamic>> result = await bancoDeDados
         .query(_tablename, where: '$_name=?', whereArgs: [nomeDaTarefa]);
-  print("tarefa encontrada: ${toList(result)}");
-  return toList(result);
+    print("tarefa encontrada: ${toList(result)}");
+    return toList(result);
   }
 
-  delete(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {
+    print("deletendo tarefa $nomeDaTarefa");
+    final Database bancoDeDados = await getDatabasase();
+    return bancoDeDados
+        .delete(_tablename, where: '$_name = ?', whereArgs: [nomeDaTarefa]);
+  }
 }
